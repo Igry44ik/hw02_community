20f7116b6eacaa0cc9a8556b2f8f6f37ae8e5177
= <=>
2021-11-20 15:25:12 +0300

    Добавление навигации по страницам

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,17 @@
+from django.core.paginator import Paginator
+
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        "posts": posts,
+        "page_obj": page_obj,
     }
     return render(request, "posts/index.html", context)
 
